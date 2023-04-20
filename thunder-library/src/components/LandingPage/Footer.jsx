a7dcc0c844b56345e34ee9cda1181d8e8c600f34
AsherAST <damianchis2212@gmail.com>
2023-04-20 18:17:40 -0500

    create footer

--- thunder-library/src/components/LandingPage/Footer.jsx
+++ thunder-library/src/components/LandingPage/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Box } from '@mui/material';
+import { Box, Grid, Link } from '@mui/material';
+
 import { Typography } from '@mui/material';
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
   return (
     <Box sx={styles.footer}>
-        <Typography sx={styles.text}>Thunder Library © 2023</Typography>
+      <Grid sx={styles.redes}>
+         <Link href="https://www.facebook.com/" target='_blank' >
+          <FacebookIcon fontSize='large'></FacebookIcon>
+        </Link>
+        <Link href="https://www.instagram.com/" target="_blank">
+          <InstagramIcon fontSize='large'></InstagramIcon>
+        </Link>
+        <Link href="https://www.twitter.com/" target="_blank">
+          <TwitterIcon fontSize='large'></TwitterIcon>
+        </Link>
+      
+      
+      
+      </Grid>
+      <Box sx={styles.text}>
+        <Typography>Politica de Privacidad</Typography>
+        <Typography>Terminos de Uso</Typography>
+        <Typography>Thunder Library © 2023</Typography>
+      </Box>
     </Box>
   )
 }
@@ -14,14 +37,24 @@ export default Footer
 
 const styles = {
     footer: {
+        backgroundColor: "rgb(7,14,24)",
         position: "relative",
-        marginTop: "60px",
+        marginTop: "150px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    redes: {
+      backgroundColor: "rgb(7,14,24)",
+      py: 2,
+    
+      textAlign: "center",
     },
     text: {
-        position: "absolute",
-        bottom: "20px",
-        left: 0,
-        right: 0,
-        textAlign: "center",
-    }
+        py: 4,
+        display: "flex",
+        columnGap: "18rem",
+        
+    },
 }
