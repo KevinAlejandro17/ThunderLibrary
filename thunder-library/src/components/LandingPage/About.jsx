56813a3eb9c08a2b72720b5d66dc7bb11c8c774f
AsherAST <damianchis2212@gmail.com>
2023-04-26 16:27:04 -0500

    add about info

--- thunder-library/src/components/LandingPage/About.jsx
+++ thunder-library/src/components/LandingPage/About.jsx
@@ -7,7 +7,14 @@ const About = () => {
       id="about"
       sx={{ height: "80vh", display: "grid", placeItems: "center", py: 10 }}
     >
-      <Typography variant="h3">Acerca de</Typography>
+      <Typography variant="h3">Quiénes Somos</Typography>
+      <Box
+      sx={{display: 'flex', alignItems: 'center', height: '100vh', mx: 15}} 
+      >
+      <Typography sx={{textAlign: 'center'}} variant="h6">
+      Thunder library es una solución tecnológica que ayuda a las librerías a optimizar su gestión de préstamos y controlar su inventario de libros de manera más eficiente. Nuestros clientes pueden disfrutar de la comodidad de consultar en línea la disponibilidad de los libros y hacer reservas en cualquier momento y desde cualquier lugar. En Thunder library, estamos comprometidos en proporcionar una solución tecnológica que ayude a las librerías a mejorar su servicio y ofrecer una experiencia más satisfactoria a sus usuarios.
+      </Typography>
+</Box>
     </Box>
   );
 };
